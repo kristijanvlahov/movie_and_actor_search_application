1473e5ad0ce4e7a86ef2f2d7594b77c6ef452c37
kristijanvlahov <kristijanvlahov@gmail.com>
2021-09-22 21:22:03 +0200

    updated search

--- src/ActorSearch.js
+++ src/ActorSearch.js
@@ -2,50 +2,48 @@ import './App.css';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+ 
 const ActorSearch = () => {
-
+ 
   const [data, setData] = useState([]);
   const [text, setText] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [array, setArray] = useState([]);
   let actor = [];
   let actors = [];
-
+ 
   async function fetchData() {
-
+ 
     const SparqlClient = require('sparql-http-client')
-
-    const client = new SparqlClient({ endpointUrl: 'https://dbpedia.org/sparql' })
-
+ 
+    const client = new SparqlClient({ endpointUrl: 'https://query.wikidata.org/sparql' })
+ 
     const query = `
-    PREFIX dbo: <http://dbpedia.org/ontology/>
-    PREFIX dct: <http://purl.org/dc/terms/>
-    PREFIX dbc: <http://dbpedia.org/resource/Category:>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX wikibase: <http://wikiba.se/ontology#>
+    PREFIX wd: <http://www.wikidata.org/entity/> 
+    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX dbp: <http://dbpedia.org/property/>
-    SELECT DISTINCT ?actorLabel ?dob ?birthPlaceLabel ?image ?abstract WHERE{
-         ?actorLink a dbo:Actor;
-                          dbp:name ?actorLabel;
-                          dbo:birthDate ?dob;
-                          dbo:birthPlace ?birthPlace;
-                          dbo:thumbnail ?image;
-                          dbo:abstract ?abstract.
-
-        ?birthPlace rdfs:label ?birthPlaceLabel.
-        FILTER(LANG(?birthPlaceLabel)='en').
-
+    PREFIX p: <http://www.wikidata.org/prop/>
+    PREFIX v: <http://www.wikidata.org/prop/statement/>
+    SELECT DISTINCT ?image ?actorLabel ?dob ?pobLabel ?countryLabel
+WHERE {
+  ?film wdt:P31 wd:Q11424;
+        wdt:P161 ?actor.
+  ?actor wdt:P18 ?image; 
+         wdt:P569 ?dob;
+         wdt:P19 ?pob;
+         wdt:P27 ?country.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en" }
 }
-    LIMIT 100`
-
+LIMIT 100`
+ 
     const stream = await client.query.select(query);
-
+ 
     stream.on('data', row => {
       Object.entries(row).forEach(([key, value]) => {
         //console.log(`${key}: ${value.value} (${value.termType})`)
       })
-      setData(x => [...x,row]);
+      setData(x => [...x, row]);
       console.log(data);
       array.push(row.actorLabel.value);
       console.log(array);
@@ -54,20 +52,19 @@ const ActorSearch = () => {
   useEffect(() => {
     fetchData();
   }, [])
-
+ 
   const handleSearch = (text) => {
     console.log(data);
     actor = data.find(x => x.actorLabel.value === text);
     console.log(actor);
-    if(typeof(actor)=='object'){
-      actors = Object.values(actor);
+    if (typeof (actor) == 'object') {
+      actors = [...actors, actor];
       console.log(actors);
     }
-    
   }
-
+ 
   const onChangeHandler = (text) => {
-
+ 
     let matches = []
     let data2 = [];
     data2 = [...array];
@@ -80,38 +77,46 @@ const ActorSearch = () => {
     setText(text);
     setSuggestions(matches);
   }
-
+ 
   const onSuggestHandler = (text) => {
     setText(text);
     setSuggestions([]);
   }
-
+ 
   return (
     <div className="App">
+      
       <div className="container">
         <img src="https://www.nicepng.com/png/detail/153-1531345_ocean-waves-actor-logo.png" width="100px" height="100px"></img>
         <h1>Actor Search</h1>
         <Link to={'/'}>Movie Search</Link>
-        </div>
+      </div>
+      
       <div className="autocomplete">
         <input type="text" className="input" placeholder="Enter actor name"
           onChange={e => onChangeHandler(e.target.value)}
-          value={text}></input>
-
+          value={text}>
+        </input>
         <button type="submit" onClick={handleSearch(text)}>Search</button>
       </div>
-
-      {suggestions && suggestions.filter((val,i)=>i<5).map((suggestion, i) =>
+ 
+      {suggestions && suggestions.filter((val, i) => i < 5).map((suggestion, i) =>
         <div className="suggestion" onClick={() => onSuggestHandler(suggestion)}>{suggestion}
         </div>
       )}
+ 
+      {Object.keys(actors).map((item, i) => (
+        <div key={i}>
+          <h3>{actors[item].actorLabel.value}</h3>
+          <img src={actors[item].image.value} width="120px" height="150px"></img>
+          <p>Date of birth: {actors[item].dob.value}</p>
+          <p>Country: {actors[item].countryLabel.value}</p>
+          <p>Place of birth: {actors[item].pobLabel.value}</p>
+        </div>
+      ))}
       
-      {actors.map((a, i) =>
-        <li key={i}>{a.value}</li>
-      )}
-            
     </div>
   );
 }
-
+ 
 export default ActorSearch;
--- src/MovieSearch.js
+++ src/MovieSearch.js
@@ -2,23 +2,24 @@ import './App.css';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-
+ 
 const MovieSearch = () => {
-
+ 
   const [data, setData] = useState([]);
   const [text, setText] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [array, setArray] = useState([]);
   let movie = [];
   let movies = [];
-  let o = {value:"Title,Duration"}
-
+  let countries_filtered = [];
+  let countries = [];
+ 
   async function fetchData() {
-
+ 
     const SparqlClient = require('sparql-http-client')
-
+ 
     const client = new SparqlClient({ endpointUrl: 'https://dbpedia.org/sparql' })
-
+ 
     const query = `
     PREFIX dbo: <http://dbpedia.org/ontology/>
     PREFIX dct: <http://purl.org/dc/terms/>
@@ -28,50 +29,44 @@ const MovieSearch = () => {
     PREFIX dbp: <http://dbpedia.org/property/>
     SELECT DISTINCT ?title (SAMPLE(?genreLinkLabel) AS ?genreLinkLabel) (SAMPLE(?comment) AS ?comment)  (SAMPLE(?directorLabel) AS ?directorLabel) (SAMPLE(?country) AS ?country)  {
       ?movieLink a dbo:Film; 
-                        dct:subject ?genreLink; 
-                                  foaf:name ?title; 
-                        dbo:director ?director; 
-                      dbp:country ?country;
-                      rdfs:comment ?comment.
-                      
-
+                 dct:subject ?genreLink; 
+                 foaf:name ?title; 
+                 dbo:director ?director; 
+                 dbp:country ?country;
+                 rdfs:comment ?comment.
       ?genreLink rdfs:label ?genreLinkLabel.
       ?director rdfs:label ?directorLabel.
-    FILTER(LANG(?directorLabel)='en').
-    FILTER(regex(?country , "United States")).
-       
+    FILTER(LANG(?comment)='en').
     }
-    GROUP BY ?title
-    LIMIT 100
-   `
-
+    LIMIT 100`
+ 
     const stream = await client.query.select(query);
-
+ 
     stream.on('data', row => {
       Object.entries(row).forEach(([key, value]) => {
         //console.log(`${key}: ${value.value} (${value.termType})`)
       })
-      setData(x => [...x,row]);
+      setData(x => [...x, row]);
       array.push(row.title.value);
+ 
     })
   }
   useEffect(() => {
     fetchData();
   }, [])
-
+ 
   const handleSearch = (text) => {
     console.log(data);
     movie = data.find(x => x.title.value === text);
     console.log(movie);
-    if(typeof(movie)=='object'){
-      movies = Object.values(movie);
+    if (typeof (movie) == 'object') {
+      movies = [...movies, movie];
       console.log(movies);
     }
-    
   }
-
+ 
   const onChangeHandler = (text) => {
-
+ 
     let matches = []
     let data2 = [];
     data2 = [...array];
@@ -84,48 +79,84 @@ const MovieSearch = () => {
     setText(text);
     setSuggestions(matches);
   }
-
+ 
   const onSuggestHandler = (text) => {
     setText(text);
     setSuggestions([]);
   }
-
+ 
+  const filterHandler = () => {
+    var e = document.getElementById("filter");
+    var result = e.options[e.selectedIndex].value;
+ 
+    for (let i = 0; i < data.length; i++) {
+      if (result === data[i].country.value) {
+        countries_filtered.push(data[i]);
+      }
+    }
+ 
+    if (typeof (countries_filtered) == 'object') {
+      countries = [...countries, countries_filtered];
+      console.log(countries_filtered);
+      console.log(countries);
+    }
+    countries = [];
+    countries_filtered = [];
+  }
+ 
   return (
     <div className="App">
-        <div className="container">
+      <div className="container">
         <img src="https://st2.depositphotos.com/3687485/9010/v/600/depositphotos_90102796-stock-illustration-cinema-film-clapper-board-vector.jpg" width="100px" height="100px"></img>
         <h1>Movie Search</h1>
         <Link to={'/actorSearch'}>Actor Search</Link>
-        </div>
-      
+      </div>
+ 
+      <div className="filter">
+        <label>Filter by country:</label>
+ 
+        <select name="country" id="filter" onChange={filterHandler}>
+          <option value="/">Choose a country</option>
+          <option value="United States">United States</option>
+          <option value="China">China</option>
+          <option value="United Kingdom">United Kingdom</option>
+          <option value="India">India</option>
+          <option value="Australia">Australia</option>
+        </select>
+      </div>
+ 
       <div className="autocomplete">
         <input type="text" className="input" placeholder="Enter movie name"
           onChange={e => onChangeHandler(e.target.value)}
-          value={text}></input>
-
+          value={text}>
+        </input>
         <button type="submit" onClick={handleSearch(text)}>Search</button>
       </div>
-      
-      {suggestions && suggestions.filter((val,i)=>i<5).map((suggestion, i) =>
+ 
+      {suggestions && suggestions.filter((val, i) => i < 5).map((suggestion, i) =>
         <div className="suggestion" onClick={() => onSuggestHandler(suggestion)}>{suggestion}
         </div>
       )}
-
-      {movies.map((m, i) =>
-        <li key={i}>{m.value}</li>
-      )}
-
-      {/*{
-        ()=>{
-          if(movies){
-            return <li>{movies[3].value}</li>
-          }
-        }
-      }*/}
-      
-            
+ 
+      {Object.keys(movies).map((item, i) => (
+        <div key={i}>
+          <h3>{movies[item].title.value}</h3>
+          <p>Genre: {movies[item].genreLinkLabel.value}</p>
+          <p>Director: {movies[item].directorLabel.value}</p>
+          <p>Country: {movies[item].country.value}</p>
+          <p>Description: {movies[item].comment.value}</p>
+        </div>
+      ))}
+ 
+      {Object.keys(countries_filtered).map((item, i) => (
+          <li key={i}>
+          <span>{countries_filtered[item].title.value}</span>
+          </li>
+      ))}
+ 
     </div>
   );
 }
-
+ 
 export default MovieSearch;
+ 
